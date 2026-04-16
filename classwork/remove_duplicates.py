b6d100fefe3613b239f722ccdf5fe601220434a8
Ronit Gupta <139333896+RonitARK@users.noreply.github.com>
2026-04-16 10:14:33 +0530

    Implement logic to remove duplicate lines from file

--- classwork/remove_duplicates.py
+++ classwork/remove_duplicates.py
@@ -1,9 +1,11 @@
 
 # Task: Create a new file with unique lines only
 
+"""
 1. Create an empty set to track "seen" lines.
 2. Open the source file and a new file.
 3. Only write a line to the new file if it is not in the "seen" set.  
+"""
 
 seen_lines = set() # Set to store unique records
 with open('logs.txt', 'r') as infile, open('clean_logs.txt', 'w') as outfile:
