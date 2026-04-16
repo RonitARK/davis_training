13d409ebf82f92d3fef7f14161563eeb1fc67b10
Ronit Gupta <139333896+RonitARK@users.noreply.github.com>
2026-04-16 10:21:10 +0530

    Create longest_line.py

---
+++ classwork/longest_line.py
@@ -0,0 +1,17 @@
+
+# Task: Identify the longest line and its length
+
+"""
+1. Initialize a variable for the longest_line as an empty string.
+2. Iterate through every line in the file.
+3. If the current line's length is greater than the stored longest_line, update it
+"""
+
+
+longest = ""
+with open('article.txt', 'r') as f:
+    for line in f:
+        if len(line) > len(longest):
+            [span_29](start_span)longest = line # Update if longer line found
+print(f"Longest Length: {len(longest)}")
+print(f"Content: {longest.strip()}")
